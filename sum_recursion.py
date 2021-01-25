4794293a8292a30eeb5e1d71687e4345d1391841
David Alin <70276073+AlinDavid@users.noreply.github.com>
2021-01-25 17:20:14 +0200

    Create sum_recursion.py

---
+++ sum_recursion.py
@@ -0,0 +1,16 @@
+'''
+Write a program that reads values from the user until a blank line is entered. Display
+the total of all of the values entered by the user (or 0.0 if the first value entered is
+a blank line). Complete this task using recursion. Your program may not use any
+loops.
+'''
+
+def write():
+
+    user_input = input("Write a value (blank to quit) :")
+
+    if not user_input:
+        return 0
+    else:
+        return int(user_input) + write()
+   
